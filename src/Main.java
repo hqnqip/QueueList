85c7f16c0120a599436ff3135e41b002d051d03e
hqnqip <lpshbananas@gmail.com>
2025-03-21 00:32:18 -0700

    Tried adding rigorous testing.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,31 @@ public class Main
 
         a.printList();
 
+        a.deleteNode(); //Deletes 1.
+        a.deleteNode(); //Deletes 2.
+
+        System.out.println("There are " + a.amountOfNodes() + " Nodes in the QueueList.");
+
+        a.printList();
+
+        //a.addNode("a"); //Results in an error.
+
+        a.addNode(1);
+        a.addNode(2);
+
+        System.out.println("There are " + a.amountOfNodes() + " Nodes in the QueueList.");
+
+        a.printList();
+
         a.deleteNode();
+        a.addNode(5);
         a.deleteNode();
 
         System.out.println("There are " + a.amountOfNodes() + " Nodes in the QueueList.");
 
         a.printList();
+
+        a.addNode(1.0); //Does not take decimals.
+
     }
 }
